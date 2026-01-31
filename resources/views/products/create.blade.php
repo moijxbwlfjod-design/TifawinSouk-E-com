0a8e6f2c2405c9e37241acbcfb89cfbfc4e7b7dd
moijxbwlfjod-design <moijxbwlfjod@gmail.com>
2026-01-31 15:12:25 +0100

    docs: adding create template

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -10,19 +10,20 @@
         <form action="{{route('products.store')}}" method="post">
             @csrf
             <label for="name">Product Name:</label><br>
-            <input required type="text" id="name"><br><br>
+            <input name="name" required type="text" id="name"><br><br>
             <label for="reference">Reference:</label><br>
-            <input required type="text" id="reference"><br><br>
+            <input name="reference" required type="text" id="reference"><br><br>
             <label for="slug">Slug:</label><br>
-            <input required type="text" id="slug"><br><br>
+            <input name="slug" required type="text" id="slug"><br><br>
             <label for="price">price:</label><br>
-            <input required type="number" id="price"><br><br>
+            <input name="price" required type="number" id="price"><br><br>
             <label for="stock">stock:</label><br>
-            <input required type="number" id="stock"><br><br>
+            <input name="stock" required type="number" id="stock"><br><br>
             <label for="categorie_id">categorie_id:</label><br>
-            <input required type="text" id="categorie_id"><br><br>
-            <label for="image_path">Image Path:</label><br>
-            <input type="text" id="image_path"><br><br>
+            <input name="categorie_id" required type="text" id="categorie_id"><br><br>
+            <label for="image_path">Image:</label>
+            <input name="image_path" type="file" accept="image/*" id="image_path"><br><br>
+            <img src="{{asset('store/' . '}}" alt="">
             <button type="submit">Add Product</button>
         </form>
     </main>
